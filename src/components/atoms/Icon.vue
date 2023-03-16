6f899150708f4af650263ef19dd0e4481c186716
Lyokolux <lyokolux@protonmail.com>
2023-03-16 21:39:32 +0100

    feat(Icon.vue): add atom component

---
+++ src/components/atoms/Icon.vue
@@ -0,0 +1,20 @@
+<template>
+  <component :is="icons[icon]" :color="color" :size="size" :stroke-width="strokeWidth" />
+</template>
+
+<script setup lang="ts">
+import * as icons from "lucide-vue-next";
+
+type IconProps = {
+  icon: string
+  size?: string
+  color?: string
+  strokeWidth?: number
+}
+
+withDefaults(defineProps<IconProps>(), {
+  size: '1.5rem',
+  color: 'currentColor',
+  strokeWidth: 2
+})
+</script>
